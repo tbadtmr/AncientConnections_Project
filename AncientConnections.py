9c8ea8820bc1377e7bcf0e2a710fb7c29e05690b
tbadtmr <tabead16@LAPTOP-CPVDUB60>
2025-03-23 18:03:58 +0100

    App script basic outline

---
+++ AncientConnections.py
@@ -0,0 +1,50 @@
+#%% 
+# DOCUMENTATION SECTION - Ancient Connections App
+#______________________________________________________________________________
+#!/usr/bin/python3
+"""
+Description:
+
+Procedure:
+
+Usage:
+
+
+Version: 1.00
+Date: 2025-03-13
+Name: Tabea Dittmar
+"""
+
+#%%
+import streamlit as st
+import pandas as pd
+import numpy as np
+import re
+import plotly.graph_objects as go
+
+#%%
+# IMPORT DATA
+#______________________________________________________________________________
+
+#%%
+# CHOOSE SAMPLE INPUT METHOD
+#______________________________________________________________________________
+
+#%%
+# GET MOST RECENT HAPLOGROUP AND LINEAGE
+#______________________________________________________________________________
+
+
+#%%
+# FIND ANCESTORS IN DATABANK
+#______________________________________________________________________________
+
+
+# %%
+# PLOT
+#______________________________________________________________________________
+
+
+#%%
+# DISPLAY EVERYTHING
+#______________________________________________________________________________
